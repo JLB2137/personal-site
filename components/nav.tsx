7e132086bfa01079dd4fe8a45c706f8e306ac0ad
Jeremee Bornstein <jeremee.bornstein@gmail.com>
2022-02-09 13:38:04 -0500

    added tailwind functionality for built in CSS. Need to additionally add animation. Also made adjustments to navbar to utilize grid

--- components/nav.tsx
+++ components/nav.tsx
@@ -2,16 +2,22 @@ import Link from 'next/link'
 
 const Nav = () => {
     return (
-        <div className='Nav'>
-            <Link href="/">
-                <h3>Jeremee Bornstein</h3>
-            </Link>
-            <Link href="/devjourney">
-                <h3>My Development Journey</h3>
-            </Link>
-            <Link href="/hobbies/plants">
-                <h3>Plants</h3>
-            </Link>
+        <div className="font-header font-light text-4xl grid grid-cols-3">
+            <div className='justify-start'>
+                <Link href="/devjourney">
+                    <h3>Projects</h3>
+                </Link>
+            </div>
+            <div>
+                <Link href="/hobbies/plants">
+                    <h3>Plants</h3>
+                </Link>
+            </div>
+            <div className="font-name justify-self-end" >
+                <Link href="/">
+                    <h1 className="before:content-['JEREMEE'] hover:before:content-['JEREMEEEEEEE']"></h1>
+                </Link>
+            </div>
         </div>
     )
   }
