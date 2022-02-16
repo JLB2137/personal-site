96ac4de85e88e6877fd79e022d705f0567b95298
Jeremee Bornstein <jeremee.bornstein@gmail.com>
2022-02-16 12:07:18 -0500

    finished CSS basic setup with images for plants page on mobile

--- pages/hobbies/plants.tsx
+++ pages/hobbies/plants.tsx
@@ -1,17 +1,13 @@
 const Plants = () => {
     return (
         <div className="bg-zinc-600 absolute top-0">
-            <div className="absolute top-0 relative bg-zinc-600 mx-auto grid grid-rows-max grid-cols-2 sm:grid-cols-2 sm:gap-5">
+            <div className="bg-zinc-600 mx-auto grid grid-rows-max grid-cols-2 sm:grid-cols-2 sm:gap-5">
                 <img className="brightness-75 row-start-1 col-span-2 sm:rounded" src='/plants/tomatoe.png'/>
-                <img className="row-start-2 sm:rounded-xl sm:px-2" src='/plants/money/money_1.png'/>
-                <img className="row-start-2 sm:rounded-xl sm:px-2" src='/plants/palm_sm/pony_1.png'/>
-                <img className="row-start-3 sm:rounded-xl sm:px-2" src='/plants/palm_lg/tall_pony.png'/>
-                <img className="row-start-3" src='/plants/tomatoe.png'/>
-                <img className="row-start-4 rounded" src='/plants/tomatoe.png'/>
-                <img className="row-start-4" src='/plants/tomatoe.png'/>
-                <img className="row-start-5" src='/plants/tomatoe.png'/>
-                <img className="row-start-5 rounded" src='/plants/tomatoe.png'/>
-                <img className="row-start-6" src='/plants/tomatoe.png'/>
+                <img className="row-start-2 sm:rounded-xl sm:px-2 sm:h-56" src='/plants/money/money_1.png'/>
+                <img className="row-start-2 sm:rounded-xl sm:px-2 sm:h-56" src='/plants/palm_sm/pony_1.png'/>
+                <img className="row-start-3 sm:rounded-xl sm:px-2 sm:h-56" src='/plants/palm_lg/tall_pony.png'/>
+                <img className="row-start-3 sm:rounded-xl sm:px-2 sm:h-56" src='/plants/ivys.png'/>
+                <img className="row-start-4 col-span-2 sm:rounded-2xl sm:px-2 sm:mb-20" src='/plants/cactus/cactus.png'/>
             </div>
         </div>
 
