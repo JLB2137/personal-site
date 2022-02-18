9a7ce03c1427322ef8222dee09a66125a2efb292
Jeremee Bornstein <jeremee.bornstein@gmail.com>
2022-02-18 10:54:18 -0500

    stopping point to switch machines

--- pages/hobbies/plants.tsx
+++ pages/hobbies/plants.tsx
@@ -1,6 +1,6 @@
 const Plants = () => {
     return (
-        <div className="bg-zinc-600 absolute top-0">
+        <div className="bg-zinc-600 absolute top-0 absolute">
             <div className="bg-zinc-600 mx-auto grid grid-rows-max grid-cols-2 sm:grid-cols-2 sm:gap-5">
                 <img className="brightness-75 row-start-1 col-span-2 sm:rounded" src='/plants/tomatoe.png'/>
                 <img className="row-start-2 sm:rounded-xl sm:px-2 sm:h-56" src='/plants/money/money_1.png'/>
