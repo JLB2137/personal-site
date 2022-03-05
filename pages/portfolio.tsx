acf7534b0dd3c40084fec3070ae0dd57e84cb749
Jeremee <jeremee.bornstein@gmail.com>
2022-03-04 19:00:45 -0500

    resolved issues for building

--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -8,8 +8,8 @@ const Portfolio = () => {
         </div>
 
         <div className='grid grid-cols-2 justify-center mx-auto text-white'>
-          <a href="https://jlb2137.github.io/" target="_blank"><h1>Giphy Search Engine!</h1></a>
-          <a href="https://github.com/JLB2137/Giphy-API-Project" target="_blank"><Image className="invert" src="/github.png" width={25} height={25} layout="intrinsic" /></a>
+          <a href="https://jlb2137.github.io/" target="_blank" rel="noreferrer"><h1>Giphy Search Engine!</h1></a>
+          <a href="https://github.com/JLB2137/Giphy-API-Project" target="_blank" rel="noreferrer"><Image className="invert" src="/github.png" width={25} height={25} layout="intrinsic" /></a>
           <Image className='col-span-2' src="/mobile/projects/giphy-search-engine.png" width={658} height={398} layout="intrinsic" />
         </div>
       </div>
