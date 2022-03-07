d5868b7dda646f3044266cc4d76713df83ace57d
Jeremee Bornstein <jeremee.bornstein@gmail.com>
2022-03-07 18:19:03 -0500

    changed Plant PLP to add begining of sanity, added new page for plants for generic use with CMS, added client file to interact with sanity server

--- pages/hobbies/plants.tsx
+++ pages/hobbies/plants.tsx
@@ -1,31 +1,69 @@
 import Link from 'next/link';
-
+import sanity from '../../client';
+import { useEffect, useState } from "react";
 interface screenWidthProp {
     screenWidth: number
 }
 
 const Plants = (props: screenWidthProp) => {
-    return (
-        <div>
-            <div className="bg-zinc-600 absolute top-0">
-                <div className="bg-zinc-600 mx-auto grid grid-rows-max grid-cols-2 sm:grid-cols-2 sm:gap-5">
-                    <img className="brightness-75 row-start-1 col-span-2 sm:h-screen sm:w-max" src={props.screenWidth && props.screenWidth < 600 ? '/plants/tomatoe_mobile.png' : '/plants/tomatoe.png'}/>
+
+    const [plants,setPlants] = useState()
+
+    
+    useEffect(async () => {
+        setPlants(await sanity.fetch('*[_type == "plants"]'))
+        console.log(await plants)
+
+    },[])
+
+
+
+
+    const loaded = () => {
+
+        const linksInput = plants.map(plant => {
+
+            console.log(plants)
+
+            return(
+                <Link href='/hobbies/plants/[id]' as={`/hobbies/plants/${plant.slug.current}`}>
+                    <button className="italic text-white border-white hover:text-neutral-400 hover:border-neutral-400 sm:border-4 sm:rounded-md sm:text-xl sm:p-1 sm:mb-5">{plant.name}</button>
+                </Link>
+            )
+        })
+
+        return (
+            <div>
+                <div className="bg-zinc-600 absolute top-0">
+                    <div className="bg-zinc-600 mx-auto grid grid-rows-max grid-cols-2 sm:grid-cols-2 sm:gap-5">
+                        <img className="brightness-75 row-start-1 col-span-2 sm:h-screen sm:w-max" src={props.screenWidth && props.screenWidth < 600 ? '/plants/tomatoe_mobile.png' : '/plants/tomatoe.png'}/>
+                    </div>
+                </div>
+                <div className="relative grid justify-center items-center mt-10">
+                    <Link href='/hobbies/plants/money'>
+                        <button className="italic text-white border-white hover:text-neutral-400 hover:border-neutral-400 sm:border-4 sm:rounded-md sm:text-xl sm:p-1 sm:mb-5">Money Tree</button>
+                    </Link>
+                    <Link href='/hobbies/plants/palm'>
+                        <button className="italic text-white border-white hover:text-neutral-400 hover:border-neutral-400 sm:border-4 sm:rounded-md sm:text-xl sm:p-1 sm:mb-5">Ponytail Palm</button>
+                    </Link>
+                    {linksInput}
                 </div>
             </div>
-            <div className="relative grid justify-center items-center mt-10">
-                <Link href='/hobbies/plants/money'>
-                    <button className="italic text-white border-white hover:text-neutral-400 hover:border-neutral-400 sm:border-4 sm:rounded-md sm:text-xl sm:p-1 sm:mb-5">Money Tree</button>
-                </Link>
-                <Link href='/hobbies/plants/palm'>
-                    <button className="italic text-white border-white hover:text-neutral-400 hover:border-neutral-400 sm:border-4 sm:rounded-md sm:text-xl sm:p-1 sm:mb-5">Ponytail Palm</button>
-                </Link>
-            </div>
-        </div>
+        )
+    }
+
+    const loading = () => {
+        return(
+            <h1>Loading</h1>
+        )
+    }
+
+
+    return plants ? loaded() : loading()
 
         
         
-         
-    )
+
   }
 
   export default Plants
---
+++ pages/hobbies/plants/[id].tsx
@@ -0,0 +1,86 @@
+import { useEffect, useState } from "react";
+import { motion, PanInfo } from "framer-motion";
+
+
+const Plant = () => {
+
+
+    interface types {
+        imageIterator: number,
+        setImageIterator: (imageIterator: number) => void,
+        imageArray: string,
+        image: string,
+        setImage: (image: string) => void,
+        initalPosition: number,
+        setInitialPosition: (initialPosition: number) => void
+    }
+
+    const [imageIterator,setImageIterator] = useState(0)
+    const imageArray = ['/mobile/plants/money/money_1.png','/mobile/plants/money/money_2.png','/mobile/plants/money/money_3.png','/mobile/plants/money/money_4.png','/mobile/plants/money/money_5.png']
+    const [image, setImage] = useState(imageArray[0])
+    const [initialPosition, setInitialPosition] = useState(0)
+    
+    
+    const positionLocator = (info: PanInfo) => {
+        setInitialPosition(info.point.x)
+    }
+
+    const onDrag = (info: PanInfo) => {
+        //if the image is dragged to the left and is last of stack
+        if (initialPosition > info.point.x && image === imageArray[imageArray.length-1]) {
+            setImageIterator(0)
+        //if image is dragged left but isn't last of stack
+        } else if (initialPosition > info.point.x && image !== imageArray[imageArray.length-1]) {
+            //grab the last index value of the array
+            setImageIterator(imageIterator+1)
+        //if image is dragged right and is first in stack
+        } else if (initialPosition < info.point.x && image === imageArray[0]){
+            setImageIterator(imageArray.length-1)
+        //if image is dragged right and isn't end of stack
+        } else if (initialPosition < info.point.x && image !== imageArray[0]) {
+            setImageIterator(imageIterator-1)
+        }
+    }
+
+    const carouselPosition = imageArray.map(imageString => {
+        if (imageString === image) {
+            return(
+                <p className="text-white sm:mx-2">&#9679;</p>
+            )
+        } else {
+            return(
+                <p className="text-white sm:mx-2">&#9675;</p>
+            )
+        }
+
+    })
+
+
+    useEffect(()=> {
+        setImage(imageArray[imageIterator])
+    },[imageIterator])  
+
+    return (
+        <div className="pt-5">
+            <div id="header" className="grid justify-center text-white">
+                <h1 className="font-bold sm:text-3xl">Money Tree</h1>
+                <h5 className="italic sm:text-sm sm:text-center">Pachira aquatica</h5>
+            </div>
+            <div className="grid justify-center sm:pt-10 overflow-hidden">
+                <motion.div drag="x" dragConstraints={{ left: 0, right: 0 }} onDragStart={(event, info) => positionLocator(info)} onDragEnd={(event, info) => onDrag(info)}>
+                    <img className="border-white sm:border-8 sm:h-96" src={image}/>
+                </motion.div> 
+            </div>
+            <div className="flex flex-row justify-center w-max mx-auto sm:mt-4">
+                {carouselPosition}
+            </div>
+            
+        </div>
+
+        
+        
+         
+    )
+  }
+
+  export default Plant
