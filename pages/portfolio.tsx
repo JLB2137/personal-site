4f4d3aabd773034da9088bf98515095437f42d12
Jeremee <jeremee.bornstein@gmail.com>
2022-04-01 11:56:59 +0200

    added css and setup with sanity for Portfolio page on mobile

--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -1,8 +1,10 @@
 import Image from 'next/image'
+import Link from 'next/link';
 import imageUrlBuilder from '@sanity/image-url';
 import sanity from '../client';
 import {GetStaticPropsContext} from 'next';
-import {useEffect} from 'react'
+import {useState,useEffect} from 'react'
+import {motion} from 'framer-motion';
 
 interface ImagesType {
   _key: string,
@@ -58,6 +60,8 @@ export async function getStaticProps({params}:GetStaticPropsContext) {
     })
   })
 
+  console.log(portfolio)
+
 
   return {
       props: {
@@ -68,6 +72,23 @@ export async function getStaticProps({params}:GetStaticPropsContext) {
 }
 
 const Portfolio = (props: PropsType) => {
+
+  const [opacityChanger, setOpacityChanger] = useState(true)
+
+  const variants = {
+    visible: {opacity: 1, display: 'grid'},
+    darker: {opacity: .25},
+    hidden: {opacity: 0, display: 'none'},
+  }
+
+  const onTap = () => {
+    if (opacityChanger === true) {
+      setOpacityChanger(false)
+      console.log("false")
+    } else {
+      setOpacityChanger(true)
+    }
+  }
 
   
 
@@ -107,18 +128,33 @@ const Portfolio = (props: PropsType) => {
                   <Image className="invert" src="/github.png" width={25} height={25} layout="fixed" />
                 </a>
               </div>
-              <img className='col-span-2' src={projectImage} />
-              <p className='col-span-2' >{project.shortDescription[0].children[0].text}</p>
-              <p className='col-span-2 mx-auto my-1 font-semibold'>Technologies:</p>
-              <div className='col-span-2 flex flex-wrap justify-center mx-auto'>
-                {
-                  project.technology.map(technology => {
-                      return(
-                        <p className='mx-5 my-1'>{technology.children[0].text}</p>
-                      )
-                  })
-                }
-                </div>
+              <div className='relative col-span-2 flex sm:h-full'>
+                <motion.div initial="visible" animate={opacityChanger ? "visible" : "darker"} variants={variants} transition={{ type: "spring", duration: 0.8 }} onTap={onTap}>
+              
+                  <img src={projectImage} />
+
+                </motion.div>
+                <motion.div className='absolute top-0 left-0 z-10 flex-auto sm:h-full' initial="hidden" animate={opacityChanger ? "hidden" : "visible"} variants={variants} transition={{ type: "spring", duration: 0.8 }} onTap={onTap}>
+                  <div className='col-span-2 flex flex-wrap justify-center mx-auto sm:my-5 sm:text-xl'>
+                    {
+                      project.technology.map(technology => {
+                          return(
+                            <p className='bg-white text-black self-center sm:px-2 sm:mx-5'>{technology.children[0].text}</p>
+                          )
+                      })
+                    }
+                  </div>
+                  <div className='flex col-span-2'>
+                    <Link href={{
+                        pathname: `/portfolio/[id]`,
+                        query: {id: project.slug.current}
+                    }} key={project.slug.current}>
+                        <button className="italic bg-white text-black border-white mx-auto self-end hover:text-neutral-400 hover:border-neutral-400  sm:my-5 sm:w-40 sm:border-2 sm:rounded-full sm:text-2xl">Learn More</button>
+                    </Link>
+                  </div>
+                </motion.div>
+              </div>
+
             </div>
           )
         })
