032d9a80e27bfb5788adfb9bb7602d9a3f51dffb
JLB2137 <jeremee.bornstein@gmail.com>
2022-07-03 19:47:38 -0400

    made adjustments to new PLP plants page to add section for plant image name and URL in props being pulled on page load

--- pages/test/portfolio.tsx
+++ pages/test/portfolio.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link';
 import imageUrlBuilder from '@sanity/image-url';
-import sanity from '../client';
+import sanity from '../../client';
 import {GetStaticPropsContext} from 'next';
 import {useState,useEffect} from 'react'
 import {motion} from 'framer-motion';
@@ -66,6 +66,7 @@ interface Props {
   screenWidth: string,
   portfolio: ProjectType[]
   images: Images[],
+  plants: Plants[]
 }
 
 interface PlantsType {
@@ -92,8 +93,7 @@ export async function getStaticProps({params}:GetStaticPropsContext, props: Prop
   //grab all projects from Sanity
   const sanityPlants: PlantsType[] = await sanity.fetch('*[_type == "plants"]')
   //create an images array to grab all images and create links using sanity builder
-  //const [projectPLPImage,setProjectPLPImage] = useState<StateProps>([])
-  let plant: Plants[] = []
+  let plants: Plants[] = []
   let imageString: string
   let imageName: string
   sanityPlants.map(selectedPlant => {
@@ -103,7 +103,7 @@ export async function getStaticProps({params}:GetStaticPropsContext, props: Prop
       imageString = imageUrlBuilder(sanity).image(image).url()
 
       //append to images array the projectName and the image url
-      plant.push({
+      plants.push({
         plantName: selectedPlant.name,
         image: imageString,
         imageName: imageName
@@ -115,10 +115,8 @@ export async function getStaticProps({params}:GetStaticPropsContext, props: Prop
 
   return {
       props: {
-          plant,
+          plants
           //can be removed later once useState works
-          images,
-          //projectPLPImage
       }
   }
 }
@@ -126,11 +124,11 @@ export async function getStaticProps({params}:GetStaticPropsContext, props: Prop
 const Portfolio = (props: Props) => {
 
   const [opacityChanger, setOpacityChanger] = useState(true)
-  const [projectPLPImage,setProjectPLPImage] = useState(props.images)
+  const [plant,setPlant] = useState(props.plants)
 
 
   useEffect(()=> {
-    console.log("hook results", projectPLPImage)
+    console.log("hook results", plant)
   },[])
   
   
@@ -154,28 +152,27 @@ const Portfolio = (props: Props) => {
   return (
     <div className='pt-10 bg-black sm:pb-5 lg:flex lg:flex-wrap'>
       <div className='justify-left text-white lg:w-full lg:ml-32 sm:ml-5'>
-        <h1 className="font-bold lg:text-7xl sm:text-3xl h-min">Portfolio</h1>
+        <h1 className="font-bold lg:text-7xl sm:text-3xl h-min">Plants</h1>
       </div>
       { 
-        props.portfolio.map(project => {
+        props.plants.map((plant,index) => {
           //this is a placeholder value to declare something before it is updated on the render
           let imageIndex: number = 0
 
-          projectPLPImage.map((image,index) => {
-            //If viewport is mobile grab the image with mobile_PLP name
-            if (props.screenWidth === 'mobile' && image.projectName === project.projectName && image.imageName === 'mobile_PLP') {
-              imageIndex = index
-            } else if (props.screenWidth === 'desktop' && image.projectName === project.projectName && image.imageName === 'desktop_PLP') {
-              imageIndex = index
-            }
-          })
+          //If viewport is mobile grab the image with mobile_PLP name
+          if (props.screenWidth === 'mobile' && plant.imageName === 'mobile_PLP') {
+            imageIndex = index
+          } else if (props.screenWidth === 'desktop' && plant.imageName === 'desktop_PLP') {
+            imageIndex = index
+          }
+          
           return(
             <div key={project.projectName} className='justify-center mx-auto text-white lg:pt-10 sm:grid sm:grid-cols-2 sm:mt-5 sm:h-max'>
               <div className='flex relative lg:flex-wrap sm:col-span-2 sm:h-full'>
                 {props.screenWidth === 'mobile' ?
                   <motion.div className='px-5' initial="visible" animate={opacityChanger ? "visible" : "darker"} variants={variants} transition={{ type: "spring", duration: 0.8 }} onTap={onTap}>
                     <div className='flex mx-auto'>
-                      <Image src={projectPLPImage[imageIndex].image} width={props.imageWidthPLP} height={props.imageHeightPLP} layout="intrinsic"/>
+                      <Image src={props.plants[imageIndex].image} width={props.imageWidthPLP} height={props.imageHeightPLP} layout="intrinsic"/>
                     </div>
                   </motion.div>
 
@@ -183,7 +180,7 @@ const Portfolio = (props: Props) => {
 
                   <motion.div className='sm:px-5' initial="visible" animate={opacityChanger ? "visible" : "darker"} variants={variants} transition={{ type: "spring", duration: 0.8 }} onHoverStart={onTap}>
                     <div className='flex mx-auto'>
-                      <Image src={projectPLPImage[imageIndex].image} width={props.imageWidthPLP} height={props.imageHeightPLP} layout="intrinsic"/>
+                      <Image src={props.plants[imageIndex].image} width={props.imageWidthPLP} height={props.imageHeightPLP} layout="intrinsic"/>
                     </div>
                   </motion.div>
 
@@ -191,10 +188,12 @@ const Portfolio = (props: Props) => {
                 {props.screenWidth === 'mobile' ?
                   <motion.div className='absolute top-0 left-0 z-10 flex-auto sm:h-full' initial="hidden" animate={opacityChanger ? "hidden" : "visible"} variants={variants} transition={{ type: "spring", duration: 1 }} onTap={onTap}>
                     <div className='flex col-span-2 mx-auto items-center lg:text-3xl lg:my-5 sm:mb-3'>
+
+
+Need to adjust this so it says the plant name but anchor tag is removed because we are not redirecting to a project site.
+
+
                       <a className='underline mx-auto sm:text-2xl sm:mx-1' href={project.projectURL} target="_blank" rel="noreferrer">{project.projectName}</a>
-                      <a className='sm:mx-1 flex items-center' href={project.gitURL} target="_blank" rel="noreferrer">
-                        <Image className="invert" src="/github.png" width={25} height={25} layout="fixed" />
-                      </a>
                     </div>
                     <div className='col-span-2 flex flex-wrap justify-center mx-auto sm:my-5 sm:text-lg'>
                         {
